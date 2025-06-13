80720e3ea3dc799acf6ab831304089b89656cf35
enigmaapos <apostoliaenigma@gmail.com>
2025-06-13 09:57:27 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { AlertTriangle, AlertCircle } from "lucide-react";
-import { Alert, AlertDescription, AlertTitle } from "../components/Alert";
+
+
 
 
 
