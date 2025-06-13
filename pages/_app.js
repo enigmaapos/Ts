ee3098db5f85b1444ada471f02fe4acd7a3612d1
enigmaapos <apostoliaenigma@gmail.com>
2025-06-13 09:40:58 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import '../styles/globals.css';
-import Navbar from '../components/Navbar';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
