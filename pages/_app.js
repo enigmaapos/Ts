297b7f1d2f44eb6f5e75fa58b026490157dfa07f
enigmaapos <apostoliaenigma@gmail.com>
2025-06-13 09:31:42 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import '../styles/globals.css';
 import Navbar from '../components/Navbar';
-import FloatingButton from '../components/FloatingButton';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
