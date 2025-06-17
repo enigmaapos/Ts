1d72738178ba5861b317db518d5e9aee1d2d44ac
enigmaapos <apostoliaenigma@gmail.com>
2025-06-17 18:32:58 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -328,7 +328,7 @@ const detectBullishToBearish = (
   for (let i = crossoverIndex + 1; i < len - 1; i++) {
     const nearEMA = highs[i] >= ema70[i] && lows[i] <= ema70[i];
     const aboveEMA = closes[i] > ema70[i];
-    const nearOrAboveEMA = nearEMA || aboveEMA;
+    const nearOrUnderEMA = nearEMA || underEMA;
 
     const fallingRSI = rsi14[i] < crossoverRSI;
     const lowerThanCrossover = closes[i] < crossoverLow;
@@ -392,7 +392,7 @@ const detectBearishToBullish = (
   for (let i = crossoverIndex + 1; i < len - 1; i++) {
     const nearEMA = highs[i] >= ema70[i] && lows[i] <= ema70[i];
     const underEMA = closes[i] < ema70[i];
-    const nearOrUnderEMA = nearEMA || underEMA;
+    const nearOrAboveEMA = nearEMA || aboveEMA;
 
     const risingRSI = rsi14[i] > crossoverRSI;
     const higherThanCrossover = closes[i] > crossoverHigh;
