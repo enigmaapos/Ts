239df136a8984b511a5ef5d6f992b2be9db52ca4
enigmaapos <apostoliaenigma@gmail.com>
2025-06-19 21:13:37 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1096,7 +1096,7 @@ if (loading) {
         </th>
         <th className="px-1 py-0.5 w-[60px] text-center">Collapse</th>
         <th className="px-1 py-0.5 w-[60px] text-center">Spike</th>
-        <th className="px-1 py-0.5 min-w-[90px] text-center">Signal</th>
+        <th className="px-1 py-0.5 min-w-[60px] text-center">Signal</th>
       </tr>
     </thead>
 
