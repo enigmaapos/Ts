9038e22729af031fe5f6bd35df3b31f6be3f227a
enigmaapos <apostoliaenigma@gmail.com>
2025-07-05 16:52:34 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1993,7 +1993,7 @@ if (loading) {
 	  
   </div>
 </div>
-
+</div>
 
 <div className="overflow-auto max-h-[80vh] border border-gray-700 rounded">
   <table className="w-full text-[11px] border-collapse">
