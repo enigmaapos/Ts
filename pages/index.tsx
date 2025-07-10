e2d1d24fff8e20a0a1374332a9939f534e88fa02
enigmaapos <apostoliaenigma@gmail.com>
2025-07-11 07:32:01 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -416,7 +416,9 @@ const PriceChangePercent = ({
 };
 
 export default function Home() {
- const [signals, setSignals] = useState<any[]>([]); // 👈 Default to empty array
+ // Option 2 (multiple signal objects):
+const [signals, setSignals] = useState<any[]>([]);
+setSignals([result]);
   const [search, setSearch] = useState("");
   const [lastUpdatedMap, setLastUpdatedMap] = useState<{ [symbol: string]: number }>({});
   const [loading, setLoading] = useState(true);
